0ad86f7a929fd9e57db0b7da9593610f30293120
jackliulovelt@outlook.com <jackliulovelt@outlook.com>
2018-05-23 00:37:18 +0800

    spider all free

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 var { getHtml, parseOriginHtml, parseTargetHtml, savefileToPath } = require('./src/spider.js')
-var originUrl = 'https://www.rails365.net/playlists/you-ren-de-react-shi-pin-jiao-cheng-ji-chu-pian'
+var originUrl = 'https://www.rails365.net/movies/q/free?_='
+// var originUrl = 'https://www.rails365.net/playlists/you-ren-de-react-shi-pin-jiao-cheng-ji-chu-pian'
 var baseUrl = 'https://www.rails365.net'
 
-getHtml(originUrl).then((data) => {
-  parseOriginHtml(data).then((targetUrls) => {
-    targetUrls.map((item) => {
-      var url = baseUrl + item
-      getHtml(url).then((data) => {
-        parseTargetHtml(data).then((data) => {
-          console.log(`${data.title} start download`)
-          getHtml(data.src, 'binary').then((fileData) => {
-            savefileToPath(data.title, fileData)
+var arr = [1, 2, 3, 4, 5, 6]
+arr.map((item) => {
+  spiderAll(`${originUrl}${(new Date()).getTime()}&page=${item}`)
+})
+
+function spiderAll(indexUrl) {
+  getHtml(indexUrl).then((data) => {
+    parseOriginHtml(data).then((targetUrls) => {
+      targetUrls.map((item) => {
+        var url = baseUrl + item
+        getHtml(url).then((data) => {
+          parseTargetHtml(data).then((data) => {
+            console.log(`${data.title} start download`)
+            getHtml(data.src, 'binary').then((fileData) => {
+              savefileToPath(data.title, fileData)
+            })
           })
         })
       })
     })
+  }).catch((err) => {
+    console.log(err)
   })
-}).catch((err) => {
-  console.log(err)
-})
+}
